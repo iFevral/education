ed1c889078427319052277ad0b4e8414defad930
Олег Педан <olehgame@ukr.net>
2019-11-11 17:36:17 +0200

    Add blanks for repos

---
+++ ClassLibrary1/Repositories/Interfaces/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1.Repositories.Base
+{
+    interface IRepository<T> : IDisposable 
+        where T : class
+    {
+        IEnumerable<T> GetBookList();
+        T GetBook(int id); 
+        void Create(T item); 
+        void Update(T item); 
+        void Delete(int id); 
+        void Save();
+    }
+}
---
+++ Store.BusinessLogic/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Store.BusinessLogic
+{
+    public class Class1
+    {
+    }
+}
