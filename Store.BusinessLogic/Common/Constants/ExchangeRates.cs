eb8a34d2ac546cb7a4a632c4e2bba1c486464f18
Олег Педан <olehgame@ukr.net>
2019-12-25 14:11:36 +0200

    fix exchange rates

--- Store.BusinessLogic/Common/Constants/ExchangeRates.cs
+++ Store.BusinessLogic/Common/Constants/ExchangeRates.cs
@@ -7,21 +7,21 @@
             public static readonly decimal[] fromUSD = 
             {
                 1m,      //USD
-                0.9m,    //EUR
-                0.77m,   //GBP
-                0.98m,   //CHF
-                109.41m, //JPY
-                23.36m,  //UAH
+                0.9009m,    //EUR
+                0.770772m,   //GBP
+                0.9811m,   //CHF
+                109.385m, //JPY
+                23.245m,  //UAH
             };
 
             public static readonly decimal[] toUSD = 
             {
                 1,      //USD
                 1.11m,   //EUR
-                1.30m,   //GBP
-                1.02m,   //CHF
-                0.0091m, //JPY
-                0.043m   //UAH
+                1.2974m,   //GBP
+                1.01926m,   //CHF
+                0.00914202m, //JPY
+                0.04302m   //UAH
             };
         }
     }
